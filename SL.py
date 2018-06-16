9bc3091e049a505ba8b34e3b2efc8f38575b9cf3
sidgharge <siddharth.gharge@bridgelabz.com>
2018-06-16 17:36:03 +0530

    all problems done

--- SL.py
+++ SL.py
@@ -5,9 +5,8 @@ from multifeaturesregression import MultiFeaturesRegression as reg
 
 iris = datasets.load_iris()
 data = iris["data"]
-y = np.array([data[:, 2]])
-x = np.transpose(data[:, 0:2])
-x = np.append(x, ([data[:, 3]]), axis=0)
+y = np.array([data[:, 0]])
+x = np.transpose(data[:, 1:4])
 w = np.zeros((x.shape[0], 1), dtype=float)
 
 r = reg(0.01, 10000, x, y)
